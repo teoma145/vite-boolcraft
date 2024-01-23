70b4de2a3f188f3e90b053759934b4291180c837
teoma145 <matteodark145@yahoo.it>
2024-01-23 15:52:43 +0100

    scaffholding iniziale finito

---
+++ src/store.js
@@ -0,0 +1,7 @@
+import axios, { Axios } from "axios";
+import { reactive } from "vue";
+
+export const store = reactive({
+    apiBaseUrl: "http://127.0.0.1:8000/api",
+    
+});
---
+++ src/views/AppHome.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+      <h1>Hello boyzzz</h1>
+    </div>
+  </template>
+  
+  <script>
+  import { store } from "../store";
+  export default {
+    name: 'AppHome',
+    data() {
+      return {
+  
+      }
+    },
+    component: {
+  
+    },
+    methods: {
+    },
+   
+  }
+  </script>
+  
+  <style lang="scss" scoped>
+ 
+  </style>
