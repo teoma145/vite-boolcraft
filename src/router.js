3e730a710636408e1b4d81437284fec5f6959ebe
burillu <burillu90@hotmail.it>
2024-01-23 17:02:16 +0100

    add types component and route

--- src/router.js
+++ src/router.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import AppHome from "./views/AppHome.vue";
+import AppTypes from "./views/AppTypes.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,7 +10,11 @@ const router = createRouter({
       name: 'home',
       component: AppHome
     },
-    
+    {
+      path: '/types',
+      name: 'types',
+      component: AppTypes
+    },
   ]
 })
 
