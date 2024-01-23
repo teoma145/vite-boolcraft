043bdec84a43de9428143e553f8234c02788e278
valeriobartoletti97 <valerio.bartoletti97@gmail.com>
2024-01-23 16:47:21 +0100

    created characters index

--- src/router.js
+++ src/router.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import AppHome from "./views/AppHome.vue";
+import AppCharacters from "./views/AppCharacters.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,7 +10,11 @@ const router = createRouter({
       name: 'home',
       component: AppHome
     },
-    
+    {
+      path: '/characters',
+      name: 'characters',
+      component: AppCharacters
+    },
   ]
 })
 
---
+++ src/views/AppCharacters.vue
@@ -0,0 +1,47 @@
+<template>
+    <h1 class="text-center text-uppercase mb-3">Characters list</h1>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4 mb-3" v-for="character in characters" :key="character.id">
+              <div class="card">
+                <div class="card-body">
+                  <h5 class="card-title">Name: {{character.name}}</h5>
+                  <p class="card-text">Description: {{ character.description }}</p>
+                  <p class="card-text">Attack Power: {{ character.attack }}</p>
+                  <p class="card-text">Defense Power: {{ character.defence }}</p>
+                  <p class="card-text">Speed: {{ character.speed }}</p>
+                  <p class="card-text">Life: {{ character.life }}</p>
+                </div>
+              </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import { store } from "../store";
+import axios from "axios";
+export default {
+  name: "AppCharacters",
+  data() {
+    return {
+      store,
+      characters: [],
+    };
+  },
+  methods: {
+    getAllCharacters() {
+      axios.get(store.apiBaseUrl + "/characters").then((response) => {
+        console.log(response);
+        this.characters = response.data.data;
+        console.log(this.characters);
+      });
+    },
+  },
+  mounted() {
+    this.getAllCharacters();
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
