cc548b88e6a79eb893da027db3c89ad8aa806085
teoma145 <matteodark145@yahoo.it>
2024-01-23 16:42:57 +0100

    Appitem-vue

---
+++ src/views/AppItem.vue
@@ -0,0 +1,41 @@
+<template>
+    <main>
+        <h1>Item List</h1>
+    
+    <div class="col-12 col-md-4 col-lg-3" v-for="item in store.Items" :key="item.id">{{ item.name }}</div>
+    </main>
+ 
+             
+ 
+ </template>
+ 
+ <script>
+  import { store } from "../store";
+  import axios from "axios";
+  
+     export default {
+         name: 'AppItem',
+         
+         data(){
+             return {
+                 store,
+
+             }
+         },
+         methods: {
+             getItems() {
+                 axios.get(store.apiBaseUrl+`/items`).then((res) => {
+                     
+                     store.Items = res.data.results
+                 })
+             }
+             },
+             created() {
+                 this.getItems();
+             }
+         }
+ </script>
+ 
+ <style lang="scss" scoped>
+ 
+ </style>
