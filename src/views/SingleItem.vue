2ea8dff7055a7f4f1f85b26ce93743e7aa767a0f
valeriobartoletti97 <valerio.bartoletti97@gmail.com>
2024-01-26 17:49:17 +0100

    items show completed

--- src/views/SingleItem.vue
+++ src/views/SingleItem.vue
@@ -1,38 +1,57 @@
 <template>
-    <main class="container">
-        <div v-if="Item">
-            <h1>{{ Item.name }}</h1>
+  <main class="container mt-5">
+    <div class="d-flex gap-5">
+      <div class="items-img" v-if="item">
+        <img src="/images/javelin.jpg" :alt="this.item.name" />
+      </div>
+      <div class="items-description">
+        <h1 class="text-center items-name py-1">{{ item.name }}</h1>
+        <p class="text-center my-4">{{ item.description }}</p>
+        <div class="text-center items-prop py-1 mb-2">Category</div>
+        <div class="text-center my-4">{{ item.category }}</div>
+        <div class="text-center items-prop py-1 mb-2">Specs</div>
+        <div class="d-flex justify-content-center align-items-center mt-3 mb-1">
+          <div class="d-flex flex-column me-4">
+            <i class="fa-solid fa-dumbbell items-icon text-center"></i>
+            <span>{{ item.weight }}</span>
+          </div>
+          <div class="d-flex flex-column">
+            <i class="fa-solid fa-coins items-icon text-center"></i>
+            <span>{{ item.cost }}</span>
+          </div>
         </div>
-    </main>
+      </div>
+    </div>
+  </main>
 </template>
 
 <script>
-import axios from 'axios';
-import { store } from '../store';
+import axios from "axios";
+import { store } from "../store";
 export default {
-    name: 'SingleItem',
-    data() {
-        return {
-            store,
-            Item: null
-        }
+  name: "SingleItem",
+  data() {
+    return {
+      store,
+      item: null,
+    };
+  },
+  methods: {
+    getItemData() {
+      axios
+        .get(`${this.store.apiBaseUrl}/items/${this.$route.params.id}`)
+        .then((res) => {
+          console.log(res.data);
+          if (res.data.result) {
+            this.item = res.data.result;
+          }
+        });
     },
-    methods: {
-        getItemData() {
-
-            axios.get(`${this.store.apiBaseUrl}/items/${this.$route.params.id}`).then((res) => {
-                console.log(res.data)
-                if (res.data.result) {
-                    this.Item = res.data.result
-                }
-
-            })
-        }
-    },
-    created() {
-        this.getItemData()
-    }
-}
+  },
+  created() {
+    this.getItemData();
+  },
+};
 </script>
 
 <style lang="scss" scoped></style>
