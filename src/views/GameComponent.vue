10a27604ada56226f8c378997e03ac47d44780c6
teoma145 <matteodark145@yahoo.it>
2024-01-26 17:24:20 +0100

    GAMELOGIC-1

--- src/views/GameComponent.vue
+++ src/views/GameComponent.vue
@@ -3,28 +3,29 @@
         <div class="container d-flex h-100 align-items-center justify-content-center ">
             <div class="d-flex">
                 <div class="text-black me-5 bg-white ">
-                    {{ getRandomCharacter(characters).name }}<br>
-                    Difesa:{{ getRandomCharacter(characters).defence }}<br>
-                    Vita:{{ getRandomCharacter(characters).life }}<br>
-                    speed:{{ getRandomCharacter(characters).speed }}<br>
-                    Attacco:{{ getRandomCharacter(characters).attack }}<br>
-                    classe:{{ getRandomType(typesList).name }}<br>
-                    oggetto:{{ getRandomItem(store.Items).name }}
+                <select v-model="selectedCharacter" @change="getSelectedCharacterStats">
+                    <option v-for="character in characters" :key="character.id" :value="character.name">{{ character.name }}</option>
+                </select><br>
+                    Difesa:{{ selectedCharacterStats.defence }}<br>
+                    Vita:{{ selectedCharacterStats.life }}<br>
+                    Speed:{{ selectedCharacterStats.speed }}<br>
+                    Attacco:{{ selectedCharacterStats.attack }}<br>
+                    
                 </div>
                 <div class="mt-4">
-                    <H1 class="text-danger">VERSUS</H1>
+                    <h1 class="text-danger">VERSUS</h1>
                 </div>
                 <div class="text-black ms-5 bg-white ">
-                    {{ getRandomCharacter(characters).name }}<br>
-                    Difesa:{{ getRandomCharacter(characters).defence }}<br>
-                    Vita:{{ getRandomCharacter(characters).life }}<br>
-                    speed:{{ getRandomCharacter(characters).speed }}<br>
-                    Attacco:{{ getRandomCharacter(characters).attack }}<br>
-                    classe:{{ getRandomType(typesList).name }}<br>
-                    oggetto:{{ getRandomItem(store.Items).name }}
+                    nome:{{ selectedOpponentStats.name }}<br>
+                    Difesa:{{ selectedOpponentStats.defence }}<br>
+                    Vita:{{ selectedOpponentStats.life }}<br>
+                    Speed:{{ selectedOpponentStats.speed }}<br>
+                    Attacco:{{ selectedOpponentStats.attack }}<br>
+                    
+                    
                 </div>
             </div>
-
+             <button @click="selectRandomOpponent">Entra nell'arena</button><br>
         </div>
     </div>
 </template>
@@ -40,15 +41,51 @@ export default {
             typesList: null,
             characters: [],
             store,
+            selectedCharacter: "",
+            selectedCharacterStats: {
+                defence: 0,
+                life: 0,
+                speed: 0,
+                attack: 0,
+                
+            },
+            selectedOpponentStats: {
+            name: "",
+            defence: 0,
+            life: 0,
+            speed: 0,
+            attack: 0,
+            
+      }
         }
     },
     methods: {
+        getSelectedCharacterStats() {
+            const selectedCharacter = this.characters.find(character => character.name === this.selectedCharacter);
+            if (selectedCharacter) {
+                this.selectedCharacterStats.defence = selectedCharacter.defence;
+                this.selectedCharacterStats.life = selectedCharacter.life;
+                this.selectedCharacterStats.speed = selectedCharacter.speed;
+                this.selectedCharacterStats.attack = selectedCharacter.attack;
+                 
+                
+            }
+        },
         getGameCharacters() {
             axios.get(store.apiBaseUrl + "/characters").then((response) => {
                 console.log(response);
                 this.characters = response.data.data;
                 console.log(this.characters);
             });
+        },
+         selectRandomOpponent() {
+        const filteredCharacters = this.characters.filter(character => character.name !== this.selectedCharacter);
+        const randomOpponent = this.getRandomCharacter(filteredCharacters);
+        this.selectedOpponentStats.name = randomOpponent.name;
+        this.selectedOpponentStats.defence = randomOpponent.defence;
+        this.selectedOpponentStats.life = randomOpponent.life;
+        this.selectedOpponentStats.speed = randomOpponent.speed;
+        this.selectedOpponentStats.attack = randomOpponent.attack;
         },
         getGameTypes() {
             axios.get(store.apiBaseUrl + '/types').then(res => {
