e009e92273d3d813e363f1f90d7bbcbac5e732d8
burillu <burillu90@hotmail.it>
2024-01-27 23:37:04 +0100

    change button with router link in home

--- src/views/AppHome.vue
+++ src/views/AppHome.vue
@@ -1,111 +1,107 @@
 <template>
   <div class="container-fluid d-flex justify-content-between align-items-center ">
-  <div class="conTain ">
-    <img src="../assets/images/project2.jpg" alt="boolcraft" class="boolImages">
+    <div class="conTain ">
+      <img src="../assets/images/project2.jpg" alt="boolcraft" class="boolImages">
+    </div>
+
+    <div>
+      <router-link :to="{ name: 'game' }" type="button" class="btn btn-outline-warning ">
+
+        <p>INSERT COIN</p>
+        <i class="fa-solid fa-circle-dollar-to-slot fa-bounce" style="color: #FFD43B;"></i>
+      </router-link>
+
+
+
+
+      <ul class="nav  icons">
+        <li class="follow">Follow Us</li>
+        <li><i class="fa-brands fa-facebook log"></i></li>
+        <li><i class="fa-brands fa-square-x-twitter log"></i></li>
+        <li><i class="fa-brands fa-youtube log"></i></li>
+        <li><i class="fa-brands fa-periscope log"></i></li>
+      </ul>
+    </div>
+
   </div>
-  
-  <div>
-   <button type="button" class="btn btn-outline-warning ">
-   
-    <p>INSERT COIN</p>
-    <i class="fa-solid fa-circle-dollar-to-slot fa-bounce" style="color: #FFD43B;"></i>
-  </button>
-  
- 
-  
-            
-            <ul class="nav  icons">
-                <li class="follow">Follow Us</li>
-                <li><i class="fa-brands fa-facebook log"></i></li>
-                <li><i class="fa-brands fa-square-x-twitter log" ></i></li>
-                <li><i class="fa-brands fa-youtube log" ></i></li>
-                <li><i class="fa-brands fa-periscope log" ></i></li>
-            </ul>
-       </div> 
-          
-  </div>
-  
 </template>
 
 <script>
 import { store } from "../store";
 
 export default {
- name: 'AppHome',
- data() {
-   return {
- 
-   }
- },
- 
+  name: 'AppHome',
+  data() {
+    return {
+
+    }
+  },
+
 }
 </script>
 
 <style lang="scss" scoped>
+.conTain {
+  height: 100vh;
+  width: 100%;
 
 
+}
 
-
-.conTain{
-    height: 100vh;
-    width: 100%;
-    
-     
-  }
-  .boolImages{
-    width: 100%;
-    height: 100%;
-    //background-repeat: no-repeat;
-    //margin-left: 22%;
-    position: relative;
-    //object-fit: cover;
-    //object-position: top;
-    background-image: url('../assets/images/project2.jpg');
+.boolImages {
+  width: 100%;
+  height: 100%;
+  //background-repeat: no-repeat;
+  //margin-left: 22%;
+  position: relative;
+  //object-fit: cover;
+  //object-position: top;
+  background-image: url('../assets/images/project2.jpg');
   background-size: cover;
 }
-.btn{
+
+.btn {
   position: absolute;
   top: 65%;
-  
+
   //width: 300px;
 
   //height: 140px;
   //background-color: rgba(80, 76, 71, 0.815);
   background-color: rgba(8, 8, 38, 0.632);
   left: 43%;
-  
-  border:none;
+
+  border: none;
   color: orange;
   font-size: 2.5em;
- 
+
   font-family: 'Black Ops One', system-ui;
-  
-}
- 
 
-.btn:hover { 
-    transition: 1s;
-    background-color: rgba(19, 19, 19, 0.849);
 }
 
-.icons{
+
+.btn:hover {
+  transition: 1s;
+  background-color: rgba(19, 19, 19, 0.849);
+}
+
+.icons {
   position: absolute;
   top: 90%;
   //margin-left: 16%;
- right: 40%;
+  right: 40%;
 }
 
-.log{
+.log {
   margin-left: 60px;
   font-size: 2.3em;
   //color: #FFD43B;
   color: black;
 }
 
-.follow{
+.follow {
   font-family: 'Black Ops One', system-ui;
   font-size: 1.5em;
   color: rgb(26, 24, 20);
 }
-
 </style>
