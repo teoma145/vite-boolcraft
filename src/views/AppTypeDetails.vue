344fcd09108ed13bcd746108dd7e10e32f2b267a
valeriobartoletti97 <valerio.bartoletti97@gmail.com>
2024-01-28 20:35:27 +0100

    added types show image

--- src/views/AppTypeDetails.vue
+++ src/views/AppTypeDetails.vue
@@ -1,48 +1,50 @@
 <template>
     <div class="container">
-        <h1 class="text-center">Type Details</h1>
-        <div class="row justify-content-center">
-
-            <div class="col-8">
-                <div class="card">
-                    <div class="card-body">
-                        <h3> {{ item.name }}</h3>
-                        <p>{{ item.description }}</p>
-
-                    </div>
-                </div>
-            </div>
+      <h1 class="text-center mb-5">Type Details</h1>
+      <div class="row justify-content-center">
+        <div class="col-6 type-details-img mb-4" v-if="type.image">
+           <img :src="store.imagesBaseUrl + type.image" :alt="type.name">
         </div>
+        <div class="col-8">
+          <div class="card">
+            <div class="card-body">
+              <h3>{{ type.name }}</h3>
+              <p>{{ type.description }}</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-</template>
-<script>
-import axios from 'axios';
-import { store } from '../store'
-
-export default {
-    name: 'AppTypeDetails',
+  </template>
+  <script>
+  import axios from "axios";
+  import { store } from "../store";
+  
+  export default {
+    name: "AppTypeDetails",
     data() {
-        return {
-            item: null
-
-        }
+      return {
+        type: null,
+        store
+      };
     },
     methods: {
-        getSingleType() {
-            console.log(this.$route);
-
-            axios.get(store.apiBaseUrl + '/types/' + this.$route.params.id).then(res => {
-                //console.log(res);
-                this.item = res.data.results;
-            })
-        }
+      getSingleType() {
+        console.log(this.$route);
+  
+        axios
+          .get(store.apiBaseUrl + "/types/" + this.$route.params.id)
+          .then((res) => {
+            //console.log(res);
+            this.type = res.data.results;
+          });
+      },
     },
-    mounted() {
-
-    },
+    mounted() {},
     created() {
-        this.getSingleType();
-    }
-}
-</script>
-<style></style>
+      this.getSingleType();
+    },
+  };
+  </script>
+  <style></style>
+  
